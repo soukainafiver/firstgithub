4ffd05f3f48e43bdd9a5db9c87b4ec793e68a800
soukaina <soukainafiver@gmail.com>
2023-01-02 16:57:54 -0500

    Initial_commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//Option + ( = {
+const express = require('express')
+const app = express()
+
+const port = 3598
+
+const movies = {
+    "titanic": "dicaprio , kate ,", 
+    "Iron man": "james , eric",
+}
+app.get("/movies/:id",(req, resp) => { 
+    if (movies[req.params.id] == undefined) { 
+        resp.send(` no movie with ${req.params.id} could be found`)
+       
+    }
+    else{resp.send(`Actors in movie ${req.params.id} :${movies[req.params.id]}`)} 
+    
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
